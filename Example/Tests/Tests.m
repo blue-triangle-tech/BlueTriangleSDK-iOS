3aa5f1e18f95e281bb8fa508a641245e6b2ac46e
Julian Wilkison <julian.wilkison@bluetriangle.com>
2020-10-09 11:20:13 -0400

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  BlueTriangleSDK-iOSTests.m
+//  BlueTriangleSDK-iOSTests
+//
+//  Created by julianduranmt@yahoo.com on 10/09/2020.
+//  Copyright (c) 2020 julianduranmt@yahoo.com. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
