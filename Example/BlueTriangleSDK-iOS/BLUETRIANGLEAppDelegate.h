195d14ef3ce794573db38f1fb6351d2734b880a5
Julian Wilkison <julian.wilkison@bluetriangle.com>
2020-10-09 09:04:18 -0400

    Initial commit

---
+++ Example/BlueTriangleSDK-iOS/BLUETRIANGLEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLUETRIANGLEAppDelegate.h
+//  BlueTriangleSDK-iOS
+//
+//  Created by julianduranmt@yahoo.com on 10/09/2020.
+//  Copyright (c) 2020 julianduranmt@yahoo.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BLUETRIANGLEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/BlueTriangleSDK-iOS/BLUETRIANGLEViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLUETRIANGLEViewController.h
+//  BlueTriangleSDK-iOS
+//
+//  Created by julianduranmt@yahoo.com on 10/09/2020.
+//  Copyright (c) 2020 julianduranmt@yahoo.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BLUETRIANGLEViewController : UIViewController
+
+@end
